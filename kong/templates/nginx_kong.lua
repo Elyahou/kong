146136a49c12c42bccbf2f4f1eb837e4756d5c98
Elyahou <elyahou.i@fiverr.com>
2021-04-14 14:31:02 +0300

    fix

--- kong/templates/nginx_kong.lua
+++ kong/templates/nginx_kong.lua
@@ -75,7 +75,7 @@ server {
 > end
 
     error_page 400 404 408 411 412 413 414 417 494 /kong_error_handler;
-    error_page 500 502 503 504                     /kong_error_handler;
+    error_page 500 502 503 504 505                     /kong_error_handler;
 
     access_log ${{PROXY_ACCESS_LOG}};
     error_log  ${{PROXY_ERROR_LOG}} ${{LOG_LEVEL}};
